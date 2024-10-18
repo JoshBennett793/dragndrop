860b95500ce97a913d99e4a762c37af45aa8bcdd
Josh Bennett <josh.bennett@farmqa.com>
2024-10-18 15:00:18 +0000

    Merged PR 1: Recreate basic example of sortable list
    Recreate basic example of sortable list

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,16 @@
+// import { inject } from 'aurelia-framework';
+import Sortable from 'sortablejs';
+
 export class App {
-  public message = 'Hello World!';
+  public message = 'SortableJS Aurelia Example';
+
+  public el: HTMLElement;
+
+  attached() {
+    this.el = document.getElementById('sortable-items');
+    if (this.el) {
+      Sortable.create(this.el);
+    }
+  }
+
 }
